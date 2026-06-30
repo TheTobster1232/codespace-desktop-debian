9435cf4e13620b23939b2d43df779183d7285d1a
TheTobster1232 <151861999+TheTobster1232@users.noreply.github.com>
2026-06-30 11:27:54 +0100

    Simplify desktop startup by removing window manager
    Removed openbox and tint2 from startup script.

--- .devcontainer/start-desktop.sh
+++ .devcontainer/start-desktop.sh
@@ -5,13 +5,9 @@ Xvfb :2 -screen 0 1280x800x24 &
 sleep 2
 export DISPLAY=:2
 
-# Start window manager
-openbox &
-sleep 1
-
-# Start panel so desktop doesn't go black
-tint2 &
-sleep 1
+# Start LXDE session (full desktop)
+lxsession &
+sleep 2
 
 # Start VNC
 x11vnc -display :2 -nopw -forever -shared -rfbport 5902 -ncache 0 &
